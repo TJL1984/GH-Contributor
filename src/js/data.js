b866c534658885643da8c43752a4990d7bde6b96
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-17 15:29:07 -0500

    set up data and ui. need to make an ajax call

--- src/js/data.js
+++ src/js/data.js
@@ -5,17 +5,29 @@
   window.contributor.findRepos = findRepos;
 
   /**
-   * param
-   * param
+   * @param {String} text Will search for repositories
+   * @param {String} token What the user will use while they search
+   * @return {Promise} Will manage the Ajax callback
    */
-  $.ajax({
-    url: 'https://api.github.com/search/repositories?q=...'
-    method: 'GET',
-    dataType: JSON,
-    headers: {
-      Authorization: 'token' + userToken,
-      'content-type': 'application/json'
+     function findRepos(text, token) {
+
+    $.ajax({
+        url: 'https://api.github.com/search/repositories?q=',
+        method: 'GET',
+        dataType: JSON,
+        headers: {
+        Authorization: "token" + api,
+          'content-type': 'application/json'
+        }
+      })
+
+      .done(function successHandler(data){
+        console.log("Yay!", data);
+      })
+
+      .fail(function failHandler(xhr){
+        console.log("No dice", xhr)
+      })
     }
-  })
 
 }());
--- src/js/ui.js
+++ src/js/ui.js
@@ -2,9 +2,17 @@
   'use strict';
 
   window.contributor = window.contributor || {};
+    console.log("Where am I?");
 
     $('#find')
-      .on('submit', function repos(event){
+      .on('submit', function findRepos(event){
         event.preventDefault();
+
+        var api = $(".api").val();
+          console.log(api);
+        var query = $(".query").val();
+          console.log(query);
+
+        window.contributor.findRepos(api, query);
       });
 }());
